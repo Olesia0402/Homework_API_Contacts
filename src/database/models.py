869c432bd2d4c89d3b78f98c57812d39c299f126
Olesia0402 <olesyashevchuk0402@gmail.com>
2024-05-26 13:05:58 +0200

    add users and authenticate

--- src/database/models.py
+++ src/database/models.py
@@ -1,8 +1,23 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, func, Table, UniqueConstraint
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now(), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+
+    contacts = relationship("Contact", back_populates="user")
 
 
 class Contact(Base):
@@ -12,6 +27,11 @@ class Contact(Base):
     last_name = Column(String(), nullable=False)
     email = Column(String(), nullable=False, unique=True)
     phone = Column(String(), nullable=False)
-    birthday = Column(DateTime())
+    birthday = Column(DateTime(), nullable=False)
     other_information = Column(String(), nullable=True)
     done = Column(Boolean, default=False)
+    created_at = Column('created_at', DateTime, default=func.now(), nullable=True)
+    update_at = Column('update_at', DateTime, default=func.now(), onupdate=func.now(), nullable=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+
+    user = relationship("User", back_populates="contacts")
--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -2,33 +2,28 @@ from typing import List
 
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact
-from src.schemas.schemas import ContactModel, ContactUpdate, ContactStatusUpdate
+from src.database.models import Contact, User
+from src.schemas.schemas import ContactCreate, ContactUpdate, ContactStatusUpdate
 
 
-async def get_contacts(skip: int, limit: int, db: Session) -> List[Contact]:
-    return db.query(Contact).offset(skip).limit(limit).all()
+async def get_contacts(skip: int, limit: int, user: User, db: Session) -> List[Contact]:
+    return db.query(Contact).filter(Contact.user_id == user.id).offset(skip).limit(limit).all()
 
 
-async def get_contact(contact_id: int, db: Session) -> Contact:
-    return db.query(Contact).filter(Contact.id == contact_id).first()
+async def get_contact(contact_id: int, user: User, db: Session) -> Contact:
+    return db.query(Contact).filter(Contact.id == contact_id, Contact.user_id == user.id).first()
 
 
-async def create_contact(body: ContactModel, db: Session) -> Contact:
-    contact = Contact(first_name=body.first_name,
-                      last_name=body.last_name,
-                      email=body.email,
-                      phone=body.phone,
-                      birthday=body.birthday,
-                      other_information=body.other_information)
+async def create_contact(body: ContactCreate, user: User, db: Session) -> Contact:
+    contact = Contact(user_id=user.id, **body.dict())
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return contact
 
 
-async def update_contact(contact_id: int, body: ContactUpdate, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def update_contact(contact_id: int, body: ContactUpdate, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(Contact.id == contact_id, Contact.user_id == user.id).first()
     if contact:
         contact.first_name=body.first_name,
         contact.last_name=body.last_name,
@@ -41,16 +36,16 @@ async def update_contact(contact_id: int, body: ContactUpdate, db: Session) -> C
     return contact
 
 
-async def update_status_contact(contact_id: int, body: ContactStatusUpdate, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def update_status_contact(contact_id: int, body: ContactStatusUpdate, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(Contact.id == contact_id, Contact.user_id == user.id).first()
     if contact:
         contact.done = body.done
         db.commit()
     return contact
 
 
-async def remove_contact(contact_id: int, db: Session)  -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def remove_contact(contact_id: int, user: User, db: Session)  -> Contact | None:
+    contact = db.query(Contact).filter(Contact.id == contact_id, Contact.user_id == user.id).first()
     if contact:
         db.delete(contact)
         db.commit()
